e3cc352264d8100b5b0638a480505bae4fb5c30c
dineshkumar24v <dineshkumar24v@gmail.com>
2026-04-26 19:08:17 +0530

    style: update background color of read user item for improved visibility

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
     gap: 14,
   },
   readUserItem: {
-    backgroundColor: "rgba(255, 255, 255, 0.08)",
+    backgroundColor: "rgba(28, 11, 179, 0.08)",
     borderWidth: 1,
     borderColor: "rgba(255, 255, 255, 0.05)",
   },
